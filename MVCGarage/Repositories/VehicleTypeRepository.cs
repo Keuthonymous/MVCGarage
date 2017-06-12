63cc9d65fa121b1456ff0aa6b5e5335518e92250
Wille Hansson <wille.multi@live.se>
2017-06-12 11:48:09 +0200

    VTR

---
+++ MVCGarage/Repositories/VehicleTypeRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MVCGarage.Models;
+using MVCGarage.DataAccess;
+
+namespace MVCGarage.Repositories
+{
+    public class VehicleTypeRepository //: IDisposable
+    {
+        private GarageContext db = new GarageContext();
+
+        
+    }
+}
